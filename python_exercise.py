15c5c1587f9238b112330fc47355edd519d105de
vedantsingh23 <141285386+vedantsingh23@users.noreply.github.com>
2023-10-11 16:34:13 -0700

    made the number list

--- python_exercise.py
+++ python_exercise.py
@@ -1,28 +1,28 @@
-# twinkle = """Twinkle, twinkle, little star,
-# 	How I wonder what you are! 
-# 		Up above the world so high,   		
-# 		Like a diamond in the sky. 
-# Twinkle, twinkle, little star, 
-# 	How I wonder what you are
-# """
+twinkle = """Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are
+"""
 
-# print(twinkle)
+print(twinkle)
 
-# user_input = input("Enter a series of numbers seperated by commas: ")
-# a = user_input.split(',')
-# List = [int(item) for item in a]
-# Tuple = tuple(int(item) for item in a)
-# print("List: " + str(List))
-# print("Tuple: " + str(Tuple))
+user_input = input("Enter a series of numbers seperated by commas: ")
+a = user_input.split(',')
+List = [int(item) for item in a]
+Tuple = tuple(int(item) for item in a)
+print("List: " + str(List))
+print("Tuple: " + str(Tuple))
 
-# common1 = input("Enter the numbers for your first list: ")
-# common2 = input("Enter the numbers for your second list: ")
-# common = []
-# for i in common1:
-# 	if i in common2 and i in common1:
-# 		common.append([i])
+common1 = input("Enter the numbers for your first list: ")
+common2 = input("Enter the numbers for your second list: ")
+common = []
+for i in common1:
+	if i in common2 and i in common1:
+		common.append([i])
 
-# print(common)
+print(common)
 
 letter = input("Enter a letter: ")
 vowels = ["a", "e", "i", "o", "u"]
@@ -32,4 +32,18 @@ for i in letter:
     else:
         print("Your letter is not a vowel")
 
+numbers = [    
+    386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 
+    399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 
+    815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717, 
+    958,743, 527
+    ]
+n2 = []
 
+for i in numbers:
+    if i in numbers:
+        n2.append([i])
+        if i == 237:
+            print(n2)
+            break
+            
