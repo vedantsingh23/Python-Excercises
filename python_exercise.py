b9623b98a15c15706a48aef1a51d826ada65ef7e
vedantsingh23 <141285386+vedantsingh23@users.noreply.github.com>
2023-10-11 16:27:55 -0700

    made the common number checker

--- python_exercise.py
+++ python_exercise.py
@@ -1,17 +1,29 @@
-twinkle = """Twinkle, twinkle, little star,
-	How I wonder what you are! 
-		Up above the world so high,   		
-		Like a diamond in the sky. 
-Twinkle, twinkle, little star, 
-	How I wonder what you are
-"""
+# twinkle = """Twinkle, twinkle, little star,
+# 	How I wonder what you are! 
+# 		Up above the world so high,   		
+# 		Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+# 	How I wonder what you are
+# """
 
-print(twinkle)
+# print(twinkle)
 
-user_input = input("Enter a series of numbers seperated by commas: ")
-a = user_input.split(',')
-List = [int(item) for item in a]
-Tuple = tuple(int(item) for item in a)
-print("List: " + str(List))
-print("Tuple: " + str(Tuple))
+# user_input = input("Enter a series of numbers seperated by commas: ")
+# a = user_input.split(',')
+# List = [int(item) for item in a]
+# Tuple = tuple(int(item) for item in a)
+# print("List: " + str(List))
+# print("Tuple: " + str(Tuple))
+
+# common1 = input("Enter the numbers for your first list: ")
+# common2 = input("Enter the numbers for your second list: ")
+# common = []
+# for i in common1:
+# 	if i in common2 and i in common1:
+# 		common.append([i])
+
+# print(common)
+
+
+
 
